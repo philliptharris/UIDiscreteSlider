e56b9527c4d143435838664feb379e0b7969f96b
Phillip Harris <phillip.harris@atlanticbt.com>
2014-04-23 10:08:38 -0400

    Initial Commit

---
+++ UIDiscreteSlider/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIDiscreteSlider
+//
+//  Created by Phillip Harris on 4/23/14.
+//  Copyright (c) 2014 Phillip Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIDiscreteSlider/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UIDiscreteSlider
+//
+//  Created by Phillip Harris on 4/23/14.
+//  Copyright (c) 2014 Phillip Harris. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
